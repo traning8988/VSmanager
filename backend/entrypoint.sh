78e6184de0700a26f600941fc259efec67bfbcca
Shota Nakamichi <s02178906@gmail.com>
2025-01-09 17:36:48 +0900

    dockerfileに追記

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
+
+RUN bundle exec rails db:create
+RUN bundle exec rails db:migrate
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
