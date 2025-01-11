b3e392b54473d23823cc40c3f0086d7f43d71d7a
Shota Nakamichi <s02178906@gmail.com>
2025-01-11 13:08:28 +0900

    entrypointの設定追加

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,5 +7,6 @@ rm -f /app/tmp/pids/server.pid
 
 bundle exec rails db:create || true
 bundle exec rails db:migrate || true
+bundle exec rails db:seed || true
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
