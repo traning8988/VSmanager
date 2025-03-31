310ac52accaf67459f540037e54e5812ce64bb42
traning8988 <142848531+traning8988@users.noreply.github.com>
2025-03-31 17:24:44 +0900

    型定義をまとめる

--- frontend/types/match.ts
+++ frontend/types/match.ts
@@ -1,8 +1,29 @@
-export interface Match {
-  message: string;
+export type MatchRecord = {
+  team1_common: string;
+  team2_common: string;
+  place: string;
+  times: string;
   league: string;
   date: string;
+}
+
+export type MatchRequestsIndex = {
+  id: number | string;
+  common_name: string;
+  category: string;
+  division: number;
+  requested_date: string;
+  double_header: boolean;
+}
+
+export type MatchingIndex = {
+  id: number | null;
+  league: string;
+  date: string;
+  time: string;
   place: string;
   team1: string;
   team2: string;
-}
+  team1_score: number | null;
+  team2_score: number | null;
+};
