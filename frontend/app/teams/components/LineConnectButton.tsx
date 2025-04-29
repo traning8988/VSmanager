6b50b9523e5210c5a5c850a9c3f2e4a019b09102
traning8988 <142848531+traning8988@users.noreply.github.com>
2025-04-29 10:00:28 +0900

    lineConectButtonをリダイレクトに変更

--- frontend/app/teams/components/LineConnectButton.tsx
+++ frontend/app/teams/components/LineConnectButton.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import liff from "@line/liff";
 import { useAtom } from "jotai";
 import { lineIdAtom } from "@/app/utils/store/atoms";
 import { toast } from "react-toastify";
@@ -8,19 +7,10 @@ import { Button } from "@/components/ui/button";
 
 export const LineConnectButton = () => {
   const [lineId] = useAtom(lineIdAtom);
-  const ConnectLiff = async () => {
-    if (lineId !== null) {
-      console.log("LINEID", lineId);
-      toast("LINE連携済みです。");
-      return;
-    }
-    await liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_CHANNEL_ID! });
-      
-    if (!liff.isLoggedIn()) {
-      liff.login({redirectUri: `${window.location.origin}/line-callback`});
-      return
-    }
-  }
+  const ConnectLiff = () => {
+    const liffId = process.env.NEXT_PUBLIC_LIFF_CHANNEL_ID!;
+    window.location.href =  `https://miniapp.line.me/${liffId}`;
+  };
 
   return (
     <Button onClick={ConnectLiff} className="bg-green-500 text-black" disabled={lineId !== null}>
