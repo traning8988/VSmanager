5f57d628d6178d8bb2ba0bce6e48a1cb728306b6
traning8988 <142848531+traning8988@users.noreply.github.com>
2025-06-17 23:31:46 +0900

    lintによるリファクタリング

--- frontend/app/admin/match-results/page.tsx
+++ frontend/app/admin/match-results/page.tsx
@@ -1,20 +1,21 @@
-import ResultsTable from './ResultsTable';
+import ResultsTable from './results-table';
+import { type MatchResult } from '@/types/match';
 
 export default async function MatchResults() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/match_results`, {
-    cache: 'no-store',
-  });
-  if (!res.ok) {
-    throw new Error('データの取得に失敗しました');
-  }
-  const matchResults = await res.json();
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/match_results`, {
+  //   cache: 'no-store',
+  // });
+  // if (!res.ok) {
+  //   throw new Error('データの取得に失敗しました');
+  // }
+  // const matchResults = await res.json();
 
   return (
     <div>
-      <ResultsTable matchResults={matchResults} />
+      {/* <ResultsTable matchResults={matchResults} />
       <br />
       <br />
-      {/* <ResultsForm matchResults={matchResults} /> */}
+      <ResultsForm matchResults={matchResults} /> */}
     </div>
   );
 }
--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import SignOut from './SignOut';
+import SignOut from './sign-out';
 import { useAtom } from 'jotai/react';
 import { isLoggedInAtom, teamIdAtom } from '../utils/store/atoms';
 import {
